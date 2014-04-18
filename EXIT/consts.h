68bf9fa391e9c4664da11e5242b4f643c2a15c9f
Guibert Timothé <timothe.guibert@outlook.fr>
2014-04-18 21:41:50 +0200

    Create consts.h
    Les constantes. Actuellement sert juste à définir une taille pour la fenêtre. Adapter à mon PC actuelle (sinon ne fonctionne pas), à modifier pour plein écran et le jeu en lui-même.

---
+++ EXIT/consts.h
@@ -0,0 +1,11 @@
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
+
+    #define SQUARE_PIX	42
+    #define SQUARE_NBW  13
+    #define SQUARE_NBH  13
+    #define W_WIN	SQUARE_PIX  * SQUARE_NBW
+    #define H_WIN       SQUARE_PIX  * SQUARE_NBH
+
+#endif
+
