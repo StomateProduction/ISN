af5674b980ce56c034b02a5dd6f7f5ddbe103565
Guibert Timothé <timothe.guibert@outlook.fr>
2014-04-22 10:47:09 +0200

    Update gamec.c

--- EXIT/gamec.c
+++ EXIT/gamec.c
@@ -1,26 +1,72 @@
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include <SDL.h>
+
 #include <SDL_image.h>
+
 #include "const.h"
+
 #include "gamec.h"
 
+
+
 void gamec(SDL_Surface* screen)
+
 {
+
     SDL_Surface *gamecs;
+
     SDL_Event event;
-    gamecs = IMG_Load("NDY.jpeg");
+
+    gamecs = IMG_Load("L0.png");
+
     int quit = 1;
 
+
+
     while (quit)
+
     {
+
         SDL_WaitEvent(&event);
+
 	switch(event.type)
+
         {
+
             case SDL_QUIT:
+
                 quit = 0;
+
                 break;
+
 	}
-    }
+
+    }  
+
+    SDL_Flip(screen);
+
+    gamecs = IMG_Load("L1.png");
+
+    SDL_Flip(screen);
+
+    gamecs = IMG_Load("L2.png");
+
+    SDL_Flip(screen);
+
+    gamecs = IMG_Load("L3.png");
+
+    SDL_Flip(screen);
+
+    gamecs = IMG_Load("L4.png");
+
+    SDL_Flip(screen);
+//fonction d'execution d'un programme à faire.
+
+
+
     SDL_FreeSurface(gamecs);
+
 }
