49676d26f7afa030307d71b07ce35fd00fcc2665
Guibert Timothé <timothe.guibert@outlook.fr>
2014-04-22 10:41:46 +0200

    Create rpg.h

---
+++ EXIT/console/rpg.h
@@ -0,0 +1,12 @@
+// THE GAME
+
+#ifndef RPG_H_INCLUDED
+# define RPG_H_INCLUDED
+
+void	my_putchar(char c);
+void	my_putstr(char *str);
+int	printArray(char **array);
+int	wallGenerator(char **array);
+int	mapGenerator(char **array);
+
+#endif /* RPG_INCLUDED_H_ */
